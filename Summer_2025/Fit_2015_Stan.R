a35ec48c98b19c34d08c7e5dd21e20665058d06f
OwenWard <owengward@gmail.com>
2025-10-18 13:20:01 -0700

    modufy to save the true xi and eta

--- Summer_2025/Fit_2015_Stan.R
+++ Summer_2025/Fit_2015_Stan.R
@@ -19,8 +19,8 @@ stan_data <- list(N = data$n_sample,
                   idx = data$known_pops,
                   p = 3,
                   known_prev = sum(data$true_subpops[data$G1_ind]/data$n_population),
-                  xi = xi, 
-                  eta = eta_vec)
+                  xi = data$xi, 
+                  eta = data$eta_vec)
 
 stan_file_2015 <- here("stan_models", "mc_cormick_and_zheng_2015.stan")
 mod_2015 <- cmdstan_model(stan_file = stan_file_2015)
