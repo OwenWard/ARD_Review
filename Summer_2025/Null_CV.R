74d645e618006aebc498d9b8820a25f4db8f592d
OwenWard <owengward@gmail.com>
2025-10-26 11:04:22 -0700

    don't run this here

--- Summer_2025/Null_CV.R
+++ Summer_2025/Null_CV.R
@@ -25,7 +25,7 @@ stan_data <- list(N = data$n_sample,
 y_sim <- stan_data$y
 G1_ind <- stan_data$idx
 Pg1 <- stan_data$known_prev
-y_folds <- kfold_split_random(K = 10, N = nrow(y_sim) * ncol(y_sim))
+# y_folds <- kfold_split_random(K = 10, N = nrow(y_sim) * ncol(y_sim))
 # saveRDS(y_folds, file = here("Summer_2025", "log_lik", "latent_sim_folds.RDS"))
 y_folds <- readRDS(file = here("Summer_2025", "log_lik", "latent_sim_folds.RDS"))
 
